5303e37eadd5aec86e5f2fad209c0b3686a931e2
reclursive <imafraidofferrets@gmail.com>
2024-04-07 13:16:14 -0400

    images still large

--- front-end/ijudge-front-vite/src/App.jsx
+++ front-end/ijudge-front-vite/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './header/header'
 import { Hero } from './hero-section/hero'
